67d99c2ff405cb7a4a4a7f6fcfbeda2471e3dc45
Martin Englund <martin@englund.nu>
2014-12-26 22:24:06 +0100

    add redis

--- Vagrantfile
+++ Vagrantfile
@@ -2,9 +2,12 @@
 # vi: set ft=ruby :
 
 Vagrant.configure(2) do |config|
-  config.vm.box = "skalera/dev"
+  config.vm.box = 'skalera/dev'
   config.vm.hostname = 'dev'
-  config.vm.network "private_network", type: 'dhcp'
-  config.vm.provision "shell", path: 'scripts/docker.sh'
+  config.vm.network 'private_network', type: 'dhcp'
+  config.vm.provision 'shell', path: 'scripts/docker.sh'
+
+  # if you want to test a docker repo from your host, e.g. influxdb, uncomment this:
+  # config.vm.synced_folder "../influxdb-docker/", "/docker"
 end
 
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,6 +4,9 @@ set -e
 
 IP=`ifconfig eth1 | grep 'inet addr' | sed 's/.*addr:\([0-9.]*\) .*/\1/'`
 
+# generated using
+# docker run --rm progrium/consul cmd:run ${IP} -d
+
 docker run \
     --name consul \
     --restart=always \
@@ -28,3 +31,17 @@ docker run -d \
     --name registrator \
     progrium/registrator consul://${IP}:8500
 
+mkdir -p /data/redis
+chown -R vagrant:vagrant /data
+
+# for more config options, see:
+# https://registry.hub.docker.com/_/redis/
+#
+docker run -d \
+    --name redis \
+    --restart=always \
+    -h $HOSTNAME \
+    -v /data/redis:/data \
+    -p 6379:6379 \
+    -e "SERVICE_TAGS=persistent" \
+    redis
