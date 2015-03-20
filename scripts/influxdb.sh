3ce805bd057886fc0662a7dc22ff797a34cfca06
Martin Englund <martin@englund.nu>
2015-03-20 13:49:59 -0700

    update influxdb

--- scripts/influxdb.sh
+++ scripts/influxdb.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-mkdir -p /data/influxdb/log
+INFLUX_DIR=/data/influxdb
+mkdir -p ${INFLUX_DIR}/log ${INFLUX_DIR}/raft ${INFLUX_DIR}/db
 cp /vagrant/config/influxdb.toml /data/influxdb/config.toml
-chown -R vagrant:vagrant /data
+chown -R vagrant:vagrant ${INFLUX_DIR}
 
 docker run -d \
-    --name influxbd \
+    --name influxdb \
     --restart=always \
-    -v /data/influxdb:/data \
+    -v ${INFLUX_DIR}:/data \
     -p 8083:8083 \
     -p 8086:8086 \
     skalera/influxdb
