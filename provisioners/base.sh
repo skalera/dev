40aa010682516a684324f8c0c96f875c3e1b3100
Martin Englund <martin@englund.nu>
2015-08-26 14:04:38 -0700

    include uuid in dev image

--- provisioners/base.sh
+++ provisioners/base.sh
@@ -10,7 +10,7 @@ if [ ! -e /usr/lib/apt/methods/https ]; then
 	apt-get install -y apt-transport-https
 fi
 
-apt-get -qq -y install linux-headers-$(uname -r) vim curl libreadline6-dev mosh
+apt-get -qq -y install linux-headers-$(uname -r) vim curl libreadline6-dev mosh uuid
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
